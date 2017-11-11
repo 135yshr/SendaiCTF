8c6b1e082eb7167b79b75778df89267ad79ef9c8
isago@praisefirst.jp <>
2017-11-11 13:43:08 +0900

    add memo area

--- sql/create_app_tables.sql
+++ sql/create_app_tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '',
   `password` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `memo` text COLLATE utf8_bin,
   `delete_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
