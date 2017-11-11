0a646eafd39513fd22090e4c1a614a354e633938
isago@praisefirst.jp <>
2017-11-11 15:44:02 +0900

    add logs tables

--- sql/create_app_tables.sql
+++ sql/create_app_tables.sql
@@ -14,3 +14,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `delete_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+--
+-- Table structure for table `logs`
+--
+CREATE TABLE `logs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `host` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `method` varchar(5) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `path` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `code` int(3) NOT NULL,
+  `referer` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `size` int(11) NOT NULL,
+  `user` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `agent` varchar(400) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
